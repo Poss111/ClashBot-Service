812c254d92994ac920fc379e90e877b109caa1ce
Daniel Poss <rixxroid@gmail.com>
2025-02-20 07:41:34 -0600

    Update .gitignore, modify build-image.sh argument, and adjust CIDR block in Terraform configuration

--- terraform/pre/main.tf
+++ terraform/pre/main.tf
@@ -78,6 +78,6 @@ resource "aws_security_group" "ecs_task_security_group" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
