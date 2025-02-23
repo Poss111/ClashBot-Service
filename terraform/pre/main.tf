45bc0e5ff60c7817a0e6f0abadc81ab1a7ad470d
Daniel Poss <rixxroid@gmail.com>
2025-02-23 02:09:59 -0600

    Remove unnecessary port specifications from ECS task security group egress rule

--- terraform/pre/main.tf
+++ terraform/pre/main.tf
@@ -121,8 +121,6 @@ resource "aws_security_group" "ecs_task_security_group" {
 resource "aws_vpc_security_group_egress_rule" "ecs_task_security_group_egress_rule" {
   security_group_id = aws_security_group.ecs_task_security_group.id
   ip_protocol       = "-1"
-  from_port         = 0
-  to_port           = 65535
   cidr_ipv4         = "0.0.0.0/0"
 }
 
